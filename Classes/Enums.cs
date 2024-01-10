2be4d810777a5b596a90275ca6655220f2b00318
AmirHosseinFarahani <dfamir2000@gmail.com>
2024-01-10 14:39:29 +0330

    Auth

---
+++ Classes/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Arad.Classes
+{
+    public class Enums
+    {
+        public enum Roles
+        {
+            Admin = 1,
+            Client = 2
+        }
+    }
+}
